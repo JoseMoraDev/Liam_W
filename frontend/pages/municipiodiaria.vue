fe1c52c2d3bf90848e2db4c621d137d4edffe875
JoseMoraCode <josemoraguitar@gmail.com>
2025-09-23 06:31:51 +0200

    datos fake para frontal

---
+++ frontend/pages/municipiodiaria.vue
@@ -0,0 +1,233 @@
+<template></template>
+
+<script>
+const data = [
+  {
+    fecha: "2025-09-23",
+    probPrecipitacion: [
+      {
+        value: 100,
+        periodo: "00-24",
+      },
+      {
+        value: 100,
+        periodo: "00-12",
+      },
+      {
+        value: 50,
+        periodo: "12-24",
+      },
+      {
+        value: 100,
+        periodo: "00-06",
+      },
+      {
+        value: 35,
+        periodo: "06-12",
+      },
+      {
+        value: 40,
+        periodo: "12-18",
+      },
+      {
+        value: 10,
+        periodo: "18-24",
+      },
+    ],
+    estadoCielo: [
+      {
+        value: "25",
+        periodo: "00-24",
+        descripcion: "Muy nuboso con lluvia",
+      },
+      {
+        value: "25",
+        periodo: "00-12",
+        descripcion: "Muy nuboso con lluvia",
+      },
+      {
+        value: "24",
+        periodo: "12-24",
+        descripcion: "Nuboso con lluvia",
+      },
+      {
+        value: "24n",
+        periodo: "00-06",
+        descripcion: "Nuboso con lluvia",
+      },
+      {
+        value: "15",
+        periodo: "06-12",
+        descripcion: "Muy nuboso",
+      },
+      {
+        value: "44",
+        periodo: "12-18",
+        descripcion: "Nuboso con lluvia escasa",
+      },
+      {
+        value: "14",
+        periodo: "18-24",
+        descripcion: "Nuboso",
+      },
+    ],
+    temperatura: {
+      maxima: 26,
+      minima: 18,
+      dato: [
+        {
+          value: 19,
+          hora: 6,
+        },
+        {
+          value: 26,
+          hora: 12,
+        },
+        {
+          value: 22,
+          hora: 18,
+        },
+        {
+          value: 18,
+          hora: 24,
+        },
+      ],
+    },
+  },
+  {
+    fecha: "2025-09-24",
+    probPrecipitacion: [
+      {
+        value: 0,
+        periodo: "00-24",
+      },
+      {
+        value: 0,
+        periodo: "00-12",
+      },
+      {
+        value: 0,
+        periodo: "12-24",
+      },
+    ],
+    estadoCielo: [
+      {
+        value: "17",
+        periodo: "00-24",
+        descripcion: "Nubes altas",
+      },
+      {
+        value: "12",
+        periodo: "00-12",
+        descripcion: "Poco nuboso",
+      },
+      {
+        value: "17",
+        periodo: "12-24",
+        descripcion: "Nubes altas",
+      },
+    ],
+    temperatura: {
+      maxima: 26,
+      minima: 15,
+      dato: [],
+    },
+  },
+  {
+    fecha: "2025-09-25",
+    probPrecipitacion: [
+      {
+        value: 5,
+        periodo: "00-24",
+      },
+      {
+        value: 5,
+        periodo: "00-12",
+      },
+      {
+        value: 0,
+        periodo: "12-24",
+      },
+    ],
+    estadoCielo: [
+      {
+        value: "14",
+        periodo: "00-24",
+        descripcion: "Nuboso",
+      },
+      {
+        value: "14",
+        periodo: "00-12",
+        descripcion: "Nuboso",
+      },
+      {
+        value: "17",
+        periodo: "12-24",
+        descripcion: "Nubes altas",
+      },
+    ],
+    temperatura: {
+      maxima: 27,
+      minima: 16,
+      dato: [],
+    },
+  },
+  {
+    fecha: "2025-09-26",
+    probPrecipitacion: [
+      {
+        value: 5,
+      },
+    ],
+    estadoCielo: [
+      {
+        value: "12",
+        descripcion: "Poco nuboso",
+      },
+    ],
+    temperatura: {
+      maxima: 29,
+      minima: 16,
+      dato: [],
+    },
+  },
+  {
+    fecha: "2025-09-27",
+    probPrecipitacion: [
+      {
+        value: 0,
+      },
+    ],
+    estadoCielo: [
+      {
+        value: "12",
+        descripcion: "Poco nuboso",
+      },
+    ],
+    temperatura: {
+      maxima: 29,
+      minima: 15,
+      dato: [],
+    },
+  },
+  {
+    fecha: "2025-09-28",
+    probPrecipitacion: [
+      {
+        value: 10,
+      },
+    ],
+    estadoCielo: [
+      {
+        value: "13",
+        descripcion: "Intervalos nubosos",
+      },
+    ],
+    temperatura: {
+      maxima: 28,
+      minima: 17,
+      dato: [],
+    },
+  },
+];
+</script>
---
+++ frontend/pages/nivologica.vue
@@ -0,0 +1,9 @@
+<template></template>
+
+<script>
+const data = {
+  zona: "Pirineo Navarro y Aragonés",
+  boletin:
+    "\nAgencia Estatal de Meteorología\n\nInformación nivológica para el Pirineo navarro y aragonés\n\nElaborado el día 19 de mayo de 2025\n\nEl 19 de mayo se da por finalizada la campaña de predicción del\npeligro de aludes en el Pirineo navarro aragonés. No obstante,\nsi las condiciones meteorológicas así lo determinaran, se\nemitiría una predicción especial. Se recuerda que, siempre que\nhaya nieve, la ausencia total de peligro no existe. Hay que\ntener presente que en circunstancias desfavorables, con aludes\nde tamaño 1 (alud pequeño o colada) y tamaño 2 (alud mediano),\nse pueden sufrir severos daños personales.\n",
+};
+</script>
